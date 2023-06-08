2190420a6b06934d97896434bfeb2b0e0ac9b800
Tituz175 <tobititus01@gmail.com>
2023-06-07 18:26:40 -0600

    task 4 modified in line with the requirement

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-number = 0
-while number != 99:
-    print(f"{number} = {hex(number)}")
-    number += 1
+for number in range(99):
+    print("{} = {}".format(number, hex(number)))
