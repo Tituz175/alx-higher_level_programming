b7e54bafab4199bb146ed2d691089d13dbe84997
Tituz175 <tobititus01@gmail.com>
2023-06-08 20:36:27 -0600

    task 2 and 3 modified inline with the task requirement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
     if argc - 1 > 1:
-        print("{:d} arguments:".format(argc - 1))
+        print("{} arguments:".format(argc - 1))
         for i in range(1, argc):
-            print("{:d}: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]))
     elif argc - 1 == 1:
-        print("{:d} argument:".format(argc - 1))
-        print("{:d}: {}".format(argc - 1, sys.argv[argc - 1]))
+        print("{} argument:".format(argc - 1))
+        print("{}: {}".format(argc - 1, sys.argv[argc - 1]))
     else:
-        print("{:d} arguments".format(argc - 1))
+        print("{} arguments".format(argc - 1))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
     result = 0
-    if argc -1 > 0:
+    if argc - 1 > 0:
         for i in range(1, argc):
             result += int(sys.argv[i])
     print("{:d}".format(result))
