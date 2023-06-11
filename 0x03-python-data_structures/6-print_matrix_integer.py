d95e7049a9159ef21e4edb32a71c2eccba7f27ab
Tituz175 <tobititus01@gmail.com>
2023-06-10 23:28:39 -0600

    task 6 modified inline with the requirement again

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            if row.index(col) == 2:
-                print("{:d}".format(col), end="")
-            else:
-                print("{:d}".format(col), end=" ")
-        print()
+    if matrix:
+        for row in matrix:
+            for col in row:
+                if row.index(col) == 2:
+                    print("{:d}".format(col), end="")
+                else:
+                    print("{:d}".format(col), end=" ")
+            print()
