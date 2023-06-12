4f998d799efec6ef05dd7789c832d17bc90cc7e5
Tituz175 <tobititus01@gmail.com>
2023-06-11 19:05:38 -0600

    take 7 modified inline with the requirement

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if tuple_a:
         if len(tuple_a) > 1:
-            a1, a2 = tuple_a
+            a1, a2 = tuple_a[0], tuple_a[1]
         else:
             a1 = tuple_a[0]
             a2 = 0
@@ -12,7 +12,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if tuple_b:
         if len(tuple_b) > 1:
-            b1, b2 = tuple_b
+            b1, b2 = tuple_b[0], tuple_b[1]
         else:
             b1 = tuple_b[0]
             b2 = 0
--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 add_tuple = __import__('7-add_tuple').add_tuple
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
+tuple_a = (1, 89, 100, 200)
+tuple_b = (88, 11, 40, 30)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
