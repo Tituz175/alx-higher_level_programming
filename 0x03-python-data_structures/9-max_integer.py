26b193a5b54f98ea03ea1d42a516a55739e3eeca
Tituz175 <tobititus01@gmail.com>
2023-06-11 19:20:42 -0600

    task 9 done

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max_value = my_list[0]
+        for num in range(1, len(my_list)):
+            if my_list[num] > max_value:
+                max_value = my_list[num]
+        return max_value
+    else:
+        return None
