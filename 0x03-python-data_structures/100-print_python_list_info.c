c882bd680d6e7576fc988a9f4eadb256cebe972e
Tituz175 <tobititus01@gmail.com>
2023-06-13 19:40:12 -0600

    task 14 function modified

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -12,14 +12,12 @@
 void print_python_list_info(PyObject *p)
 {
 	int count;
-	PyListObject *l;
 
-	l = (PyListObject *) p;
-	printf("Hello World\n");
-	printf("[*] Size of the Python List = %ld\n", l->ob_base.ob_size);
-	printf("[*] Allocated = %ld\n", l->allocated);
+	PyListObject *l = (PyListObject *)p;
+	printf("[*] Size of the Python List = %li\n", l->ob_base.ob_size);
+	printf("[*] Allocated = %li\n", l->allocated);
 	for (count = 0; count < l->ob_base.ob_size; count++)
 	{
-		printf("Element %d: %s\n", count, l->ob_item[count]->ob_type->tp_name);
+		printf("Element %i: %s\n", count, l->ob_item[count]->ob_type->tp_name);
 	}
 }
