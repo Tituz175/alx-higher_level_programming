ef7696c7d6755a1c8a7260e3a1bf67c1151ae850
Tituz175 <tobititus01@gmail.com>
2023-06-14 17:58:13 -0600

    task 1 and 2 modified inline with requirement

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if my_list:
+        my_set = {i for i in my_list}
         total = 0
-        for num in set(my_list):
+        for num in my_set:
             total += num
         return total
