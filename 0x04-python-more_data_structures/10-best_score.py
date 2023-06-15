479ab0cd05c2f98acb4c83ae23ab4e1de103d73e
Tituz175 <tobititus01@gmail.com>
2023-06-14 20:15:02 -0600

    task 10 modified inline with the requirement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary:
         best_score = max(a_dictionary.values())
-        for k,v in a_dictionary.items():
+        for k, v in a_dictionary.items():
             if v == best_score:
                 return k
     else:
