9eb6f42e9810335f4a4057e322264cdc6aa192a7
Tituz175 <tobititus01@gmail.com>
2023-06-26 19:28:41 -0600

    task 1 modfied inline with requirement

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        state = isinstance(value,int)
         print("{:d}".format(value))
-        return (state)
+        return True
     except ValueError:
-        return (state)
-
+        return False
