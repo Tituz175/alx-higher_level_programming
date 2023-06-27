7e180e91063788040b6bf283ab4ec4e814886881
Tituz175 <tobititus01@gmail.com>
2023-06-26 20:23:02 -0600

    task 4 done

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    for num in range(list_length):
+        result = 0
+        try:
+            result = my_list_1[num]/my_list_2[num]
+        except TypeError:
+             print("wrong type")
+        except ZeroDivisionError:
+             print("division by 0")
+        except IndexError:
+                print("out of range")
+        finally:
+            result_list.append(result)
+    return (result_list)
