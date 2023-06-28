58a2e9387b6c6fe62c661756fdd14fafe2a54051
Tituz175 <tobititus01@gmail.com>
2023-06-28 17:52:28 -0600

    task 4 done

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This is a module built for Square class"""
+
+
+class Square:
+    """This class represents a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """This function returns the size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This function returns the set the size property"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """This function returns the area of the square"""
+        return self.__size**2
