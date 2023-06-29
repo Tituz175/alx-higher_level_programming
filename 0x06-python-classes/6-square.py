5ba2202ce0dabc7c038b43bc9cea54c8bccaa362
Tituz175 <tobititus01@gmail.com>
2023-06-28 18:22:43 -0600

    task 6 done

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""This is a module built for Square class"""
+
+
+class Square:
+    """This class represents a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        x, y = position
+        if x < 0 or y < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    @property
+    def size(self):
+        """This function returns the size property"""
+        return self.__size
+
+    @property
+    def position(self):
+        """This function returns the position property"""
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """This function set the size property"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """This function set the position property"""
+        x, y = value
+        if x < 1 or y < 1:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """This function returns the area of the square"""
+        return self.__size**2
+
+    def my_print(self):
+        """This function print # for the shape of a square"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for rows in range(self.__size):
+                for i in range(self.__position[0]):
+                    print("_", end="")
+                for col in range(self.__size):
+                    print("#", end="")
+                print()
