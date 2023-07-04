abd5090f7fb08e2d7e287e35fac8535dbb5e23df
Tituz175 <tobititus01@gmail.com>
2023-07-04 16:15:41 -0600

    task 30 modified inline with the requirement

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""This module defines a locked class"""
+
+
 class LockedClass:
+    """
+    This class only allows instatiation of an attribute called first_name
+    """
     __slots__ = ["first_name"]
