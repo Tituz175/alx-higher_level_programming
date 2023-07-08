aaf7f722776c17a4cb8ed0513ab75a3d8d4127f1
Tituz175 <tobititus01@gmail.com>
2023-07-07 22:09:33 -0600

    task 5 modified inline with the requirement

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-This module contains a function that prints a text with 2 new lines after each of these characters: ., ? and :
+This module contains a function that prints a text with 2 new
+lines after each of these characters: ., ? and :
 """
 
 
 def text_indentation(text):
     """
-    This function prints a text with 2 new lines after each of these characters: ., ? and :
+    This function prints a text with 2 new lines after each
+    of these characters: ., ? and :
 
     Args:
         text (str): The text to be indented
