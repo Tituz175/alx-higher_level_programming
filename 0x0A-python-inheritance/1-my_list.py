06f1783d88147b0aaff99715d1fa0040799ffb73
Tituz175 <tobititus01@gmail.com>
2023-07-10 18:32:00 -0600

    task 1 modified inline with the requirement

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,10 +4,6 @@
 
 class MyList(list):
     """The my list class"""
-    def __init__(self):
-        """The constructor for mylist"""
-        list.__init__(self)
-
     def print_sorted(self):
         """this method prints the sorted list"""
         print(sorted(self))
