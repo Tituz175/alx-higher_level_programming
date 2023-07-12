d515842fab2dcd8b96d6dde4a32e10c583fb309b
Tituz175 <tobititus01@gmail.com>
2023-07-11 19:46:14 -0600

    task 5 function mode changed to 'a' for 'w'

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ def save_to_json_file(my_obj, filename):
     """
     function that writes an Object to a text file, using a JSON representation
     """
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         f.write(json.dumps(my_obj))
