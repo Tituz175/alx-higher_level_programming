6c2148f76f3ffa080d9a245cafc9349cd09216b6
Tituz175 <tobititus01@gmail.com>
2023-07-11 18:44:06 -0600

    task 0 modified inline with the requirement

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """Module read from a file"""
 
 
-def read_file(filename=""):
+def read_file(filename="", encoding="utf-8"):
     """This function reads a file and prints it to stdout"""
     with open(filename) as f:
         print(f.read())
