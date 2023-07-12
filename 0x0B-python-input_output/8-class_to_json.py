2c15263b09588f7bc40ede6926ec44ca56d33351
Tituz175 <tobititus01@gmail.com>
2023-07-11 20:30:41 -0600

    task 8 function mode changed to 'a' for 'w'

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """this module contains function"""
-import json
 
 
 def class_to_json(obj):
@@ -9,4 +8,4 @@ def class_to_json(obj):
     with simple data structure (list, dictionary, string,
     integer and boolean) for JSON serialization of an object
     """
-    return json.loads(json.dumps(obj.__dict__))
+    return obj.__dict__
