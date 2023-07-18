c95effc7751f623531a5a35712245b10abdc7b71
Tituz175 <tobititus01@gmail.com>
2023-07-17 22:13:19 -0600

    task 20 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """This is a Python package base file"""
 import json
 import os
+import csv
 
 
 class Base:
@@ -79,5 +80,56 @@ class Base:
                     cls.create(**obj) for obj
                     in cls.from_json_string(json_data)
                 ]
+        else:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Saves a list of objects to a CSV file.
+
+        Args:
+            cls: The class of the objects.
+            list_objs: A list of objects to be saved.
+        """
+
+        file_name = f"{cls.__name__}.csv"
+        with open(file_name, "w") as new_file:
+            if list_objs:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                my_Objlist = [obj.to_dictionary() for obj in list_objs]
+                csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+
+                for line in my_Objlist:
+                    csv_writer.writerow(line)
+            else:
+                new_file.write("[]")
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Loads objects from a CSV file and returns
+        a list of instantiated objects.
+
+        Returns:
+            A list of instantiated objects loaded from the CSV file.
+        """
+
+        file_name = f"{cls.__name__}.csv"
+        if os.path.exists(file_name):
+            with open(file_name, "r") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                csv_reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+                new_obj = [
+                    dict([k, int(v)] for k, v in dic.items())
+                    for dic in csv_reader
+                ]
+                return [cls.create(**obj) for obj in new_obj]
         else:
             return []
