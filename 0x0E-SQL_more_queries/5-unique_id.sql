ccb35b4af17d425c059c2e623d297e6e96cfc02f
Tituz175 <tobititus01@gmail.com>
2023-08-16 21:03:44 -0600

    tasks 5 done

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -6,7 +6,7 @@
 --      The database name will be passed as an argument of the mysql command
 --      If the table id_not_null already exists, your script should not fail
 
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT default 1 unique,
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
