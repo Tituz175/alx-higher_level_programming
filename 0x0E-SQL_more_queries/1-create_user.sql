c95010ddaa0ee5541f1e37b7757e655d2bef41bc
Tituz175 <tobititus01@gmail.com>
2023-08-16 20:48:16 -0600

    tasks 1 modified

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,4 +5,4 @@
 --      If the user user_0d_1 already exists, your script should not fail
 
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost;
+GRANT ALL ON *.* TO user_0d_1@localhost
