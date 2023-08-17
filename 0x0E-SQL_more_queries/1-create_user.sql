79922131baa31ff3ce3466861c847d2a0a67655b
Tituz175 <tobititus01@gmail.com>
2023-08-16 20:47:40 -0600

    tasks 1 modified

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,6 +4,5 @@
 --      The user_0d_1 password should be set to user_0d_1_pwd
 --      If the user user_0d_1 already exists, your script should not fail
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost;
