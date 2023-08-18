4fec684506d1f1a0b50378a20fe59a0c0a9e2dac
Tituz175 <tobititus01@gmail.com>
2023-08-17 21:08:28 -0600

    task 8 done of SQL introduction

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+-- A script that lists all rows of the table first_table from
+-- the database hbtn_0c_0 in your MySQL server.
+--      All fields should be printed
+--      The database name will be passed as an argument of the mysql command
+SELECT count(*)
+FROM first_table
+WHERE first_table.id = 89;
