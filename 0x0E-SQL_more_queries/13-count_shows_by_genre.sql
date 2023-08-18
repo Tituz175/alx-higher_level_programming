01e6601be5c38a8e978396dbd1d249306bb7eee0
Tituz175 <tobititus01@gmail.com>
2023-08-17 20:47:20 -0600

    task 13 modified again

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,8 +6,9 @@
 --      Results must be sorted in descending order by the number of shows linked
 --      You can use only one SELECT statement
 --      The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name, count(tv_show_genres.genre_id) as number_of_shows
+SELECT tv_genres.name,
+    count(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres on tv_show_genres.genre_id = tv_genres.id
+    INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
-ORDER BY number_of_shows DESC;
+ORDER BY number_of_shows DESC
