de683f78c522fa35e3a3914fbdfdab0e5cee07a6
tituz175 <tobititus01@gmail.com>
2023-09-13 20:54:24 -0600

    task 11 modified & task 12 done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,19 @@
 #!/usr/bin/node
 
-const number_list = process.argv.slice(2, -1);
-
-if (number_list.length > 1) {
-  max_number = number_list.reduce((a, b) => (a > b ? a : b));
-  number_list.splice(number_list.indexOf(max_number), 1);
-  console.log(number_list.reduce((a, b) => (a > b ? a : b)));
-} else {
+const argv = process.argv;
+const count = argv.length;
+if (count <= 3) {
   console.log(0);
+} else {
+  let max = Number(argv[2]);
+  let max1 = Number(argv[2]);
+  for (let i = 2; i < count; i++) {
+    if (Number(argv[i]) > max) {
+      max1 = max;
+      max = Number(argv[i]);
+    } else if (Number(argv[i]) > max1) {
+      max1 = Number(argv[i]);
+    }
+  }
+  console.log(max1);
 }
