bf4a0b2abc16b87b81e0c971caf18bff65e1c1ac
tituz175 <tobititus01@gmail.com>
2023-09-16 16:46:09 -0600

    task 2  refactored

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-
 """
 This module is a python script that takes in an argument and displays all
 values in the states table of hbtn_0e_0_usa where name matches the argument
- provided via the command line
+provided via the command line
 """
 
+
 if __name__ == "__main__":
-    import MySQLdb
     import sys
+    import MySQLdb
 
+    host = "localhost"
     user = sys.argv[1]
-    passwd = sys.argv[2]
-    dbname = sys.argv[3]
-    search_str = sys.argv[4]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search_state = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", user=user, passwd=passwd,
-                         port=3306, charset="utf8", db=dbname)
+    db = MySQLdb.connect(
+        host=host, port=3306, user=user, password=password, db=database,
+        charset="utf8"
+        )
 
-    query = """SELECT * FROM states """\
-        """WHERE name LIKE BINARY %s """\
-        """ORDER BY id;"""
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id".format(search_state)
 
     cursor = db.cursor()
-    cursor.execute(query, (search_str, ))
+    cursor.execute(query)
     rows = cursor.fetchall()
-
     for row in rows:
         print(row)
+
+    cursor.close()
+    db.close()
