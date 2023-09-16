20fdda2b9e009a096eeac351f362258445fb3202
tituz175 <tobititus01@gmail.com>
2023-09-16 16:19:39 -0600

    task 2 done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This is a simple script to print the state given."""
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    host = "localhost"
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search_state = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host=host, port=3306, user=user, password=password, db=database
+        )
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+
+    cursor = db.cursor()
+    cursor.execute(query, (search_state, ))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
