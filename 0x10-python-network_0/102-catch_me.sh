2a5775960974317a5e5806448233548a3467ecdd
tituz175 <tobititus01@gmail.com>
2023-09-28 22:46:57 -0600

    task 9 done python network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends request to that URL/catch_me, data in JSON
-curl -sX PUT -H "user_id=98" "0.0.0.0:5000/catch_me"
+curl -sXL PUT -H "user_id=98" "0.0.0.0:5000/catch_me"
