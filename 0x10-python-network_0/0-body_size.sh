31ae77a81dfca9922734a96601d73e7e81648f19
tituz175 <tobititus01@gmail.com>
2023-09-28 20:45:09 -0600

    task 0 modified python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cURL b0dy siz3
-curl -sw '%{size_download}\n' -o /dev/null "$1"
+# takes in a URL, sends a request to that URL,
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
