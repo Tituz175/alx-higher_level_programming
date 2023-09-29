006be28081a6a1a80b5d7ff69053dc3be3a37199
tituz175 <tobititus01@gmail.com>
2023-09-28 21:10:14 -0600

    task 3 modified python network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends OPTION request to that URL,
-curl -sI -X OPTIONS "$1" | grep 'Allow' | awk '{print $2}'
+curl -sI -X OPTIONS "$1" | grep -i 'Allow' | awk '{print $2}'
