f726ab39bf9bfd53d667c82a8b0d7ae27e5a65f9
tituz175 <tobititus01@gmail.com>
2023-09-28 20:40:07 -0600

    task 0 modified python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL,
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sIw --4 "$1" | grep -i Content-Length | awk '{print $2}'
