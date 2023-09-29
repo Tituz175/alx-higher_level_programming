6b4072a24e1efcf1d861ac6aea81ac0eaed66f49
tituz175 <tobititus01@gmail.com>
2023-09-28 22:03:19 -0600

    task 8 done python network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends POST request to that URL, data in JSON
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
