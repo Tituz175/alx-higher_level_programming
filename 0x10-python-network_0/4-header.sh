b8600b9c8c97e5473e1f0920de2e63e27768267b
tituz175 <tobititus01@gmail.com>
2023-09-28 21:19:00 -0600

    task 4 done python network

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# takes in a URL, sends OPTION request to that URL,
+curl -sI "$1"/X-School-User-Id=98
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# takes in a URL, sends OPTION request to that URL,
+curl -sI -X OPTIONS "$1" | grep -i 'Allow' | awk -F ': ' '{print $2}'
