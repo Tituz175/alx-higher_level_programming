e65cf9de8098c4d209de2d13a19eea61d5b244f3
tituz175 <tobititus01@gmail.com>
2023-09-29 22:31:08 -0600

    task 6 done

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Python script that takes in a URL and an email,
+# sends a POST request to the passed URL with the email as a parameter,
+# and displays the body of the response (decoded in utf-8)
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    data = {'email': sys.argv[2]}
+    res = requests.post(sys.argv[1], data=data)
+    print(res.text)
