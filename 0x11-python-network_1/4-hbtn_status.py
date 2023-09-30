93748c53a677ef173aea89f696a850252a9dec9a
tituz175 <tobititus01@gmail.com>
2023-09-29 22:23:07 -0600

    task 4 done

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(res.content.decode('utf-8'))}")
+    print(f"\t- content: {res.content.decode('utf-8')}")
